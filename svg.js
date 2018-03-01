04de6c2c1fc2dd8d17f292bc4f1473371b084c6a
soe43 <soe433@gmail.com>
2018-02-28 23:09:11 -0500

    finished work

--- svg.js
+++ svg.js
@@ -1,52 +1,55 @@
 var board=document.getElementById("board");
 var clearbutt=document.getElementById("clear");
-var ns="https://www.w3.org/2000/svg";
+var ns="http://www.w3.org/2000/svg";
 
 var previousX;
 var previousY;
-
-var circleNum = 0;
-
-var drawLine = function(x1,y1,x2,y2){
-    var line = document.createElementNS(ns, "line");
-    line.setAttribute("x1", x1);
-    line.setAttribute("y1", y1);
-    line.setAttribute("x2", x2);
-    line.setAttribute("y2", y2);
-    board.appendChild(line);
-}
+var started=false;
 
 var drawCircle = function(e){
-    if(circleNum == 0){
+    if(started){
 	var mouseX = e.offsetX;
 	var mouseY = e.offsetY;
-	previousX = mouseX;
-	previousY = mouseY:
-	var circ = document.createElementNS(ns, "circle");
-	circ.setAttribute("cx", mouseX);
-	circ.setAttribute("cy", mouseY);
-	circ.setAttribute("r", 25);
-	circ.setAttribute("id", str(circleNum));
-	board.appendChild(circ);
-	circleNum++;
+	var line = document.createElementNS(ns, "line");
+	line.setAttribute("x1", previousX);
+	line.setAttribute("y1", previousY);
+	line.setAttribute("x2", mouseX);
+	line.setAttribute("y2", mouseY);
+	line.setAttribute("stroke", "black");
+	board.appendChild(line);
+	console.log("drew");
+	circle(e);
     }
     else{
-	var mouseX = e.offsetX;
-	var mouseY = e.offsetY;
-	drawLine(previousX, previousY, mouseX, mouseY);
-	previousX = mouseX;
-	previousY = mouseY;
-	var circ = document.createElementNS(ns, "circle");
-	circ.setAttribute("cx", mouseX);
-	circ.setAttribute("cy", mouseY);
-	circ.setAttribute("r", 25);
-	circ.setAttribute("id", str(circleNum));
-	board.appendChild(circ);
-	circleNum++;
+	circle(e);
     }
+}
+
+var circle = function(e){
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+    previousX = mouseX;
+    previousY = mouseY;
+    started=true;
+    var circ = document.createElementNS(ns, "circle");
+    circ.setAttribute("cx", mouseX);
+    circ.setAttribute("cy", mouseY);
+    circ.setAttribute("r", 25);
+    circ.setAttribute("fill", "lightsteelblue");
+    board.appendChild(circ);
 }
 
 var clear = function(){
-    
+    while(board.hasChildNodes()){
+	board.removeChild(board.childNodes[0]);
+    }
+    started=false;
+    previousX=0;
+    previousY=0;
+    console.log("erased");
 }
+
+board.addEventListener("click", drawCircle);
+clearbutt.addEventListener("click", clear);
+
 
